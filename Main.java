7e22a719a748e05d714d470500d9bcb2e4120279
hoptilho <130772245+hoptilho@users.noreply.github.com>
2023-04-14 18:05:08 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        int base = 2, exponent = 5;
+        long result = 1;
+
+        while (exponent != 0) {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("The result is " + result);
+    }
+}
